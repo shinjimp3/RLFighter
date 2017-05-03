163116c89e2b11a39eee5a35f4484453a15896c8
penpenpng <poman1638m@gmail.com>
2017-05-03 18:00:53 +0900

    add some classes

---
+++ Assets/Scripts/Penpenpng/Strategy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.Penpenpng
+{
+	abstract class Strategy
+	{
+		abstract public Actions RunStep(States states);
+	}
+}
