00ba1926eae36df29c527ef7d654cffa390b4675
penpenpng <poman1638m@gmail.com>
2017-05-04 13:05:33 +0900

    impl agentplayer2

---
+++ Assets/Scripts/Penpenpng/DoubleRiding.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.Penpenpng
+{
+	class DoubleRiding : Strategy
+	{
+		Strategy Controller;
+		Strategy Shooter;
+
+		public DoubleRiding(Strategy controller, Strategy shooter)
+		{
+			Controller = controller;
+			Shooter = shooter;
+		}
+
+		public override Actions RunStep(States states)
+		{
+			var control = Controller.RunStep(states);
+			var shoot = Shooter.RunStep(states);
+			return new Actions()
+			{
+				yaw = control.yaw,
+				speed = control.speed,
+				shoot = shoot.shoot,
+			};
+		}
+	}
+}
